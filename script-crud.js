22a4209b361ced912dfab6a9efcae570396697c7
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-12 15:52:02 -0300

    solucionar problema com hiden

--- script-crud.js
+++ script-crud.js
@@ -3,6 +3,19 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 
+const tarefas = []
+
+
 btnAdicionarTarefa.addEventListener('click', () => {
-    formAdicionarTarefa.classList.toggle('hiden')
+    formAdicionarTarefa.classList.toggle('hidden')
 })
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    listaTarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
+})
+
